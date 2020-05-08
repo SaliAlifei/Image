5eaff827ced83b7a28a0eaafa68c4fbd891e56bd
Thaly-fifi <61205135+Thaly-fifi@users.noreply.github.com>
2020-05-08 17:12:36 +0200

    Update Main.java
    ajout commentaires

--- ProjetImage/src/algorithm/Main.java
+++ ProjetImage/src/algorithm/Main.java
@@ -2,7 +2,6 @@ package algorithm;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -30,10 +29,8 @@ public class Main extends Application {
 	public static int algorithm(BufferedImage img, boolean afficheImages) throws IOException{
 		
 		BufferedImage imgNivGris = ImgUtil.duplicate(img);
-		BufferedImage imgHistProj = ImgUtil.duplicate(img);
 		BufferedImage imgSeuillage = ImgUtil.duplicate(img);
 		int nbreDeMarche = 0;
-		int seuil = 0;
 		int largeur = 5;
 		
 		
@@ -42,8 +39,6 @@ public class Main extends Application {
 		ImgUtil.enNiveauDeGris(img);
 		ImgUtil.enNiveauDeGris(imgNivGris);
 		ImgUtil.enNiveauDeGris(imgSeuillage);
-
-		// Regler le contraste / Addition
 		
 		
 		// Seuillage / Methode Otsu
@@ -51,23 +46,16 @@ public class Main extends Application {
 		img = ImgUtil.otsuMethod4(img);
 		imgSeuillage = ImgUtil.otsuMethod4(imgSeuillage);
 
-
-		// Reduction du bruit / Convolution / Gaussian blur (taille du noyau a determiner)
 		
+		//ouverture agrandi les objets noirs (réduit le bruit du fond)
+		BufferedImage imgOuverture = ImgUtil.open(imgSeuillage, 2);
 		
-		
-		// Recherche de contours / Convolution / Sobel Operator
-			// ImgUtil.convolution(imgBuff, matriceConv);
-		
-		// remplissage (correction de l'image)		
-		
-		//fermeture
-		BufferedImage imgOuverture = ImgUtil.open(nvImgConv, 2);
-		
-		//ouverture
+		//fermeture rapetisse les objets noirs (réduit le bruit des objets)
 		BufferedImage imgFermeture = ImgUtil.close(imgOuverture, 3);
 		
-		// compter le nombre de composantes connexes
+		// compter le nombre de marches 
+		// histogramme de projection, puis si 1/4 de chaque est noir (marche) on garde la ligne del'histogramme
+		//comptage du nombre de bande sur l'image résultante
 		nbreDeMarche = ImgUtil.nbLignes(imgFermeture,largeur,1000,img.getWidth()/4);
 	
 		
